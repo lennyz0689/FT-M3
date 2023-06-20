8d11feb5114b876075daa8d0d7d9279eba95ecdd
lennyz0689 <lennyz0689@gmail.com>
2023-06-20 12:29:20 -0500

    01-Node complete

--- 01-Node/homework/01 - Exercises/bash.js
+++ 01-Node/homework/01 - Exercises/bash.js
@@ -2,7 +2,22 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function bash() {
+   process.stdout.write('prompt > ')
+   process.stdin.on('data', (data) => {
+      let args = data.toString().trim().split(' ')
+      let cmd = args.shift()
+
+         commands[cmd]
+         ? commands[cmd](print, args.join(' '))
+         : print(`command not found: ${cmd}`)
+   })
+}
+
+const print = (output) => {
+   process.stdout.write(output)
+   process.stdout.write('\nprompt > ')
+}
 
 bash();
 module.exports = {
--- 01-Node/homework/01 - Exercises/commands/index.js
+++ 01-Node/homework/01 - Exercises/commands/index.js
@@ -1,21 +1,62 @@
 const fs = require("fs");
-const utils = require("../utils/request");
 const process = require("process");
+const { request } = require("../utils/request");
 
-function pwd() {}
+function pwd(print) {
+    print(process.cwd())
+}
 
-function date() {}
+function date(print) {
+    print(Date())
+}
 
-function echo() {}
+function echo(print, args) {
+    print(args)
+}
 
-function ls() {}
+function ls(print) {
+    fs.readdir('.', (error, files)=>{
+        if(error) throw Error(error)
+        print(files.join(' '))
+    })
+}
 
-function cat() {}
+function cat(print, args) {
+    fs.readFile(args, 'utf-8', (error, data)=>{
+        if(error) throw Error(error)
+        print(data)
+    })
+}
 
-function head() {}
+function head(print, args) {
+    fs.readFile(args, 'utf-8', (error, data)=>{
+        if(error) throw Error(error)
+        print(data.split('\n')[0])
+    })
+}
 
-function tail() {}
+function tail(print, args) {
+    fs.readFile(args, 'utf-8', (error, data)=>{
+        if(error) throw Error(error)
+        const lastLines = data.split('\n')
+        print(lastLines.at(-1).trim())
+    })
+}
 
-function curl() {}
+function curl(print, args) {
+    request(args, (error, response)=>{
+        if(error) throw Error(error)
+        print(response)
+    })
+}
 
-module.exports = {};
+module.exports = {
+    pwd,
+    date,
+    echo,
+    ls,
+    cat,
+    head,
+    tail,
+    curl
+};
