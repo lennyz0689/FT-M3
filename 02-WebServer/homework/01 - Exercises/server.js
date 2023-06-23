9a4f023951aff5534b3ac37617f415ca1fda5277
lennyz0689 <lennyz0689@gmail.com>
2023-06-22 19:15:42 -0500

    Complete test 02 webServer

--- 02-WebServer/homework/01 - Exercises/server.js
+++ 02-WebServer/homework/01 - Exercises/server.js
@@ -10,5 +10,18 @@ module.exports =
   http
   .createServer((request, response)=>{
     console.log(`server raised in port ${PORT}`);
+    if (request.url === '/api') {
+      fs.readFile('./utils/dogsData.json',(err, data)=>{
+        if (err) {
+          response.writeHead(404,{'Content-type':'text/plain'})
+          return response.end('json not found')
+        }else{
+          response.writeHead(200,{'Content-type':'application/json'})
+          return response.end(data)
+        }
+      })
+      return
+    }
+    
   })
   .listen(PORT, 'localhost')
